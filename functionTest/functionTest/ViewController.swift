912008ab3cd14895833781eefd8d4100fa3b83e5
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-06-07 13:14:25 +0900

    キャストして値の出力

--- functionTest/functionTest/ViewController.swift
+++ functionTest/functionTest/ViewController.swift
@@ -13,28 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        kuji()
+        
+        let kosu = 5
+        let tanka = 2300
+        let tax = 1.08
+        let price = Double(tanka * kosu) * (1 + tax)
+        // 値の出力
+        print(price)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func kuji() {
-        // 1~10の乱数を作る
-        let num = arc4random_uniform(10) + 1
-        // numの値で処理を分岐する
-        if num >= 7 {
-            // numが7以上の時
-            let msg = "あたり"
-            print(num, msg)
-        } else {
-            // numが7未満の時
-            let msg = "はずれ"
-            print(num, msg)
-        }
-    }
     
 }
 
