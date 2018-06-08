f72ebf99dc2d463b5c301617665f9235c13f4b76
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-06-08 14:36:28 +0900

    タプルの値をインデックス番号で変更

--- functionTest/functionTest/ViewController.swift
+++ functionTest/functionTest/ViewController.swift
@@ -14,10 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let kingaku = (1000, 80)
-        let price = kingaku.0
-        let tax = kingaku.1
-        print(price + tax)
+        var user = ("鈴木", 29)
+        user.1 = 30
+        print(user)
     }
     
     override func didReceiveMemoryWarning() {
