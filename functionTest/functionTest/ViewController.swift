6401cfe0cba02a366f1f830b40cb97c0af478b08
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-06-10 13:47:44 +0900

    条件式を論理式に変更

--- functionTest/functionTest/ViewController.swift
+++ functionTest/functionTest/ViewController.swift
@@ -17,12 +17,8 @@ class ViewController: UIViewController {
         let sugaku = arc4random_uniform(101)
         let eigo = arc4random_uniform(101)
         // 数学50点以上かつ英語60点以上が合格
-        if sugaku >= 50 {
-            if eigo >= 60 {
-                print("合格です")
-            } else {
-                print("不合格です")
-            }
+        if (sugaku >= 50)&&(eigo >= 60) {
+            print("おめでとう！合格")
         } else {
             print("残念、不合格")
         }
