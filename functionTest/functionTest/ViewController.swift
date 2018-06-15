f036e512acad38b7480d54dbf9b0ea92319b7301
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-06-15 19:13:53 +0900

    内側のループから外側のループをブレイクする

--- functionTest/functionTest/ViewController.swift
+++ functionTest/functionTest/ViewController.swift
@@ -14,13 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        xloop: for x in 0...3 {
-            yloop: for y in 0...3 {
-                if (x < y) {
-                    print("-------")
-                    continue xloop
+        let vlist: Array = [[4,2],[5],[9,8,10],[6,8,-9],[4,2],[9,3]]
+        // vlistを定義する
+        outloop:for alist in vlist {
+            // vlistから取り出したalistを走査する
+            inloop:for v in alist {
+                // マイナスの値があれば出力して中断する
+                if v < 0 {
+                    print(alist)
+                    break outloop
                 }
-                print((x, y))
             }
         }
     }
