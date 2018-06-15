e030518311e5be2128ecde417b4382233097d841
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-06-15 18:39:27 +0900

    合計が21になる3個の数値の組み合わせを探す

--- functionTest/functionTest/ViewController.swift
+++ functionTest/functionTest/ViewController.swift
@@ -14,13 +14,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        var tickets = 5
-        var power = 30
-        while (tickets>0)&&(power<100) {
-            tickets -= 1
-            power += 20
-        }
-        print("power \(power)、残チケット\(tickets)")
+        var a: UInt32, b: UInt32, c: UInt32
+        var total: UInt32
+        repeat {
+            a = arc4random_uniform(13) + 1
+            b = arc4random_uniform(13) + 1
+            c = arc4random_uniform(13) + 1
+            total = a + b + c
+        } while (total != 21)
+        print("\(a),\(b),\(c)")
     }
     
     override func didReceiveMemoryWarning() {
