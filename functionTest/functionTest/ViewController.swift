f775bfd5bc86db411f93b0c7a47fe17213235388
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-06-17 22:51:14 +0900

    5人の場合の料金を計算する

--- functionTest/functionTest/ViewController.swift
+++ functionTest/functionTest/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let price = price1(ninzu: 5)
+        print(price)
 
     }
     
