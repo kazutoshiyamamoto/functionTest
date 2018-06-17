cb7774a1bd97547ee1c86afc8bbe7ffc69a8a981
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-06-17 22:48:00 +0900

    3人分の料金を計算する

--- functionTest/functionTest/ViewController.swift
+++ functionTest/functionTest/ViewController.swift
@@ -14,10 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // 半分の値を出力する
-        half(num: 25)
-        half(num: 9)
-        half(num: 12)
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -25,19 +22,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func half(num: Double) {
-        // 最後に必ず実行する
-        defer {
-            print("計算終了")
-        }
-        // 処理を中断
-        guard num >= 10 else {
-            // numが10以上でない時関数から抜ける
-            return
-        }
-        // 値を半分にして出力する
-        let halfNum = num/2
-        print("\(num)の半分は\(halfNum)")
+    func price0() -> Int {
+        let ninzu = 3
+        let tanka = 1200
+        let result = tanka * ninzu
+        return result
     }
 }
 
