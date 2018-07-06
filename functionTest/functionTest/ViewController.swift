5aabd5c124804c1bed0c8662dabe6fec47b371c9
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2018-07-06 21:27:36 +0900

    合計と平均をそれぞれ出力

--- functionTest/functionTest/ViewController.swift
+++ functionTest/functionTest/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        let result = testResult(kokugo: 80, sugaku: 68, eigo: 72)
+        print("合計\(result.total)")
+        print("平均\(result.average)")
         
         
     }
